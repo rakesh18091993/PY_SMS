1934d12cc7283d35bec23c96a5e78d2305c9773b
rakesh18091993 <60153819+rakesh18091993@users.noreply.github.com>
2020-08-18 09:15:10 +0530

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,9 +1,11 @@
 import time
 import serial
 import sys
+''' 
 # To make .exe : Open cmd in location; pyinstaller --onefile -w Ssms.py
 # add message as string ex: python Ssms.py "High Pressure" +916281439495
 # Ssms.exe "High Pressure" "+916281439495" "+919848299811" "+919676760679"
+'''
 message = sys.argv[1]
 recipients = sys.argv[2:len(sys.argv)]
 
@@ -14,7 +16,7 @@ def send_sms(number):
     phone.write(b'ATZ\r')
     time.sleep(0.1)
     # to read response: print(phone.readline(15)) 15 or number depends on response
-    phone.write(b'AT+CMGF=1\r')
+    phone.write(b'AT+CMGF=1\r') # Text mode
     time.sleep(0.1)  # Use sleep  or  if b'OK in phone.readline(15)
     phone.write(b'AT+CMGS="' + number.encode() + b'"\r')
     time.sleep(0.3)
